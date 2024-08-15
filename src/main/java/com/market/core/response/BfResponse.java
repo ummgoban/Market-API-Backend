1c60f3e1a47c6044a80dc2a2d7f3d787dc8b554f
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2024-08-15 17:05:19 +0900

    project init

---
+++ src/main/java/com/market/core/response/BfResponse.java
@@ -0,0 +1,43 @@
+package com.market.core.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.market.core.code.GlobalSuccessCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import static com.market.core.code.GlobalSuccessCode.SUCCESS;
+
+/**
+ * 프로젝트 전역적으로 사용되는 REST API 응답 클래스입니다.
+ * @param <T>
+ */
+@Getter
+@JsonInclude(value = JsonInclude.Include.NON_EMPTY)
+public class BfResponse<T> {
+
+    @Schema(description = "응답 코드", example = "200")
+    private int code;
+
+    @Schema(description = "응답 메시지", example = "성공")
+    private String message;
+
+    @Schema(description = "응답 데이터")
+    private T data;
+
+    public BfResponse(T data) {
+        this.code = SUCCESS.getCode();
+        this.message = SUCCESS.getMessage();
+        this.data = data;
+    }
+
+    public BfResponse(GlobalSuccessCode statusCode, T data) {
+        this.code = statusCode.getCode();
+        this.message = statusCode.getMessage();
+        this.data = data;
+    }
+
+    public BfResponse(GlobalSuccessCode statusCode) {
+        this.code = statusCode.getCode();
+        this.message = statusCode.getMessage();
+    }
+}
---
+++ src/main/java/com/market/core/response/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.market.core.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+/**
+ * 프로젝트 전역적으로 사용되는 REST API 에러 응답 클래스입니다.
+ */
+@Getter
+@JsonInclude(value = JsonInclude.Include.NON_EMPTY)
+public class ErrorResponse {
+
+    private final int errorCode;
+    private final String errorMessage;
+
+
+    public ErrorResponse(int errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
+}
