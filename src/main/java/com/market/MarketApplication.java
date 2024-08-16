f074c29a63fd13c7419e6f8b64695718f53c6c72
leesangmin <dkwkghkdlxld2@gmail.com>
2024-08-16 16:25:19 +0900

    feature: 기존 환경 변수 파일 경로 수정 #2

--- src/main/java/com/market/MarketApplication.java
+++ src/main/java/com/market/MarketApplication.java
@@ -1,5 +1,6 @@
 package com.market;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarketApplication {
 
     public static void main(String[] args) {
+
+        initEnv();
         SpringApplication.run(MarketApplication.class, args);
+    }
+
+    /**
+     * 스프링 부트 실행 전 시스템 property를 설정한다.
+     */
+    static void initEnv() {
+        Dotenv.configure()
+                .directory("./")
+                .filename(".env")
+                .load()
+                .entries()
+                .forEach(e -> {
+                    System.setProperty(e.getKey(), e.getValue());
+                });
     }
 }
