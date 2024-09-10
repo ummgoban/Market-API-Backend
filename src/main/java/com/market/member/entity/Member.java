c41208b969ed3d5d673be1db9c35e8dc68840877
soeun2537 <soeun2537@gmail.com>
2024-09-11 03:35:27 +0900

    feat: Member 엔티티에 provider 속성 추가 #15

--- src/main/java/com/market/member/entity/Member.java
+++ src/main/java/com/market/member/entity/Member.java
@@ -1,19 +1,27 @@
 package com.market.member.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String oauthId; // 소셜 회원 고유 ID
 
-    private String name; // 이름
+    private String provider; // OAuth2 제공자 정보
 
-    private String profileImageUrl; // 프로필 이미지
+    private String name; // 사용자 이름
+
+    private String profileImageUrl; // 프로필 이미지 URL
 
     @Enumerated(EnumType.STRING)
     private RolesType roles = RolesType.ROLE_USER; // 권한
