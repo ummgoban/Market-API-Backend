a8cfdbfb886511f112e8904e438d00a259be62de
soeun2537 <soeun2537@gmail.com>
2024-09-11 12:47:03 +0900

    feat: JWT 토큰을 HTTP 헤더에 설정하는 기능 및 관련 상수 추가 #15

---
+++ src/main/java/com/market/core/security/config/JwtProperties.java
@@ -0,0 +1,8 @@
+package com.market.core.security.config;
+
+public class JwtProperties {
+
+    public static final String ACCESS_TOKEN_HEADER = "Authorization";
+    public static final String REFRESH_TOKEN_HEADER = "Refresh-Token";
+    public static final String TOKEN_PREFIX = "Bearer ";
+}
