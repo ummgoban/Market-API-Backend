7c3efba0cd0cd4618a8dd9a08388828047625907
soeun2537 <soeun2537@gmail.com>
2024-09-19 17:23:57 +0900

    fix: @Repository 어노테이션 누락 수정 #37

--- src/main/java/com/market/member/repository/MemberRepository.java
+++ src/main/java/com/market/member/repository/MemberRepository.java
@@ -3,12 +3,14 @@ package com.market.member.repository;
 import com.market.member.entity.Member;
 import com.market.member.entity.RolesType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * Member 엔티티에 대한 JPA 레포지토리입니다.
  */
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByOauthIdAndRoles(String oauthId, RolesType rolesType);
 }
