91c2f3756ddda7a3fe36caba1cd4ee369863b57f
leesangmin <dkwkghkdlxld2@gmail.com>
2024-09-21 00:01:41 +0900

    docs: API 문서화 #33

--- src/main/java/com/market/market/controller/MarketPagingController.java
+++ src/main/java/com/market/market/controller/MarketPagingController.java
@@ -1,7 +1,14 @@
 package com.market.market.controller;
 
+import com.market.core.response.BfResponse;
+import com.market.market.dto.response.MarketPagingResponse;
 import com.market.market.service.MarketPagingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,13 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/market")
 @RequiredArgsConstructor
+@Tag(name = "가게 목록 API", description = "가게 목록 조회 관련 API 입니다.")
 public class MarketPagingController {
 
     private final MarketPagingService marketPagingService;
 
+    @Operation(summary = "가게 목록", description = "커서 기반 페이지네이션으로 가게 목록을 조회합니다.")
+    @SecurityRequirements(value = {}) // no security
     @GetMapping("/paging")
-    public void findMarketByCursorId(@RequestParam("cursorId") Long cursorId,
-                                     @RequestParam("size") Integer size) {
-        marketPagingService.findMarketByCursorId(cursorId, size);
+    public ResponseEntity<BfResponse<MarketPagingResponse>> findMarketByCursorId(
+            @Parameter(description = "마지막으로 조회한 커서 ID 입니다. 가게 ID 입니다.") @RequestParam("cursorId") Long cursorId,
+            @Parameter(description = "페이지의 크기 입니다.") @RequestParam("size") Integer size) {
+
+        return ResponseEntity.ok(
+                new BfResponse<>(marketPagingService.findMarketByCursorId(cursorId, size)));
     }
 }
