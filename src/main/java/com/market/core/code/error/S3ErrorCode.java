5e9b25e8b0f0325e2e7de2d1ad38a36f4d0ec63f
soeun2537 <soeun2537@gmail.com>
2024-09-30 21:32:50 +0900

    feat: S3 설정 클래스 및 서비스 메서드, 에러 설정 추가 #51

---
+++ src/main/java/com/market/core/code/error/S3ErrorCode.java
@@ -0,0 +1,29 @@
+package com.market.core.code.error;
+
+import com.market.core.response.ErrorResponse;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 장바구니 에러 코드를 관리하는 enum 클래스입니다.
+ */
+@Getter
+public enum S3ErrorCode implements BaseErrorCode {
+
+    FILE_UPLOAD_ERROR(500, "파일 업로드 중 오류가 발생했습니다. 다시 시도해주세요.", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    private final int errorCode;
+    private final String errorMessage;
+    private final HttpStatus status;
+
+    S3ErrorCode(int errorCode, String errorMessage, HttpStatus status) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.status = status;
+    }
+
+    @Override
+    public ErrorResponse getErrorResponse() {
+        return new ErrorResponse(this.errorCode, this.errorMessage);
+    }
+}
