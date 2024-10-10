9a1f6ab95a9726e46ff1500ef88ade893157f835
leesangmin <dkwkghkdlxld2@gmail.com>
2024-10-10 18:33:01 +0900

    feat: 주문 내역 조회 요청 값 검증하는 어노테이션 생성 #53

---
+++ src/main/java/com/market/orders/annotation/ValidOrdersStatus.java
@@ -0,0 +1,20 @@
+package com.market.orders.annotation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = OrdersStatusValidator.class)
+public @interface ValidOrdersStatus {
+
+    String message() default "Invalid ordersStatus format, check API DOCS";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
