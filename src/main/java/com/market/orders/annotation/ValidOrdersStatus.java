578ede46cf7808e6806d56cab35ffd80496c460f
leesangmin <dkwkghkdlxld2@gmail.com>
2024-10-10 23:09:39 +0900

    refactor: Restful API 스펙에 따라 GET 메소드 요청 시 request body가 아닌 파라미터를 사용하도록 리팩토링 #53

--- src/main/java/com/market/orders/annotation/ValidOrdersStatus.java
+++ src/main/java/com/market/orders/annotation/ValidOrdersStatus.java
@@ -7,7 +7,7 @@ import jakarta.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Target(ElementType.FIELD)
+@Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = OrdersStatusValidator.class)
 public @interface ValidOrdersStatus {
