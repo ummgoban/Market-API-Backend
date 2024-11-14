d04a139402e3addb32e22d35f928b7a2031f4774
soeun2537 <soeun2537@gmail.com>
2024-11-14 10:47:40 +0900

    feat: Security Access Denied 예외 처리 #69

--- src/main/java/com/market/core/exception/GlobalExceptionHandler.java
+++ src/main/java/com/market/core/exception/GlobalExceptionHandler.java
@@ -35,6 +35,16 @@ public class GlobalExceptionHandler {
     }
 
     /**
+     * Security Access Denied 관련 예외 Handler
+     */
+    @ExceptionHandler(CustomSecurityException.class)
+    protected ResponseEntity<ErrorResponse> handleCustomSecurityException(CustomSecurityException exception) {
+        log.error("CustomSecurityException 발생");
+        BaseErrorCode errorCode = exception.getErrorCode();
+        return ResponseEntity.status(errorCode.getStatus()).body(errorCode.getErrorResponse());
+    }
+
+    /**
      * Jwt 관련 예외 Handler
      */
     @ExceptionHandler(JwtException.class)
---
+++ src/main/java/com/market/core/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package com.market.core.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("Access Denied: ", accessDeniedException);
+
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().print("{\"error\": \"접근 거부\", \"message\": \"권한이 없습니다.\"}");
+        response.getWriter().flush();
+    }
+}
