948a8233e591a524e3ab55e0d0f5970ef2e46244
soeun2537 <soeun2537@gmail.com>
2024-11-22 19:18:12 +0900

    feat: 회원 정보 수정 API 엔드포인트 등록 #22

--- src/main/java/com/market/core/security/config/SecurityConfig.java
+++ src/main/java/com/market/core/security/config/SecurityConfig.java
@@ -118,6 +118,7 @@ public class SecurityConfig {
                 antMatcher(GET, "/members/markets"), // 가게 목록 조회
                 antMatcher(GET, "/members/markets/likes"), // 회원 가게 찜 목록 조회
                 antMatcher(POST, "/members/device-token"), // 기기 등록 토큰 저장
+                antMatcher(PATCH, "/members"), // 회원 정보 수정
 
                 // 가게
                 antMatcher(POST, "/markets"), // 가게 등록
