af894fa9d03daeb2b6dc4fe42ecccb8473ea99a9
leesangmin <dkwkghkdlxld2@gmail.com>
2024-11-29 00:38:59 +0900

    feat: 회원 프로필 조회 API 추가 #128

--- src/main/java/com/market/member/service/MemberService.java
+++ src/main/java/com/market/member/service/MemberService.java
@@ -1,14 +1,15 @@
 package com.market.member.service;
 
-
-import com.market.core.code.error.MemberErrorCode;
 import com.market.core.exception.MemberException;
 import com.market.member.dto.request.MemberUpdateRequest;
+import com.market.member.dto.response.ProfileReadResponseDto;
 import com.market.member.entity.Member;
 import com.market.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.market.core.code.error.MemberErrorCode.NOT_FOUND_MEMBER_ID;
 
 /**
  * 회원 서비스 클래스입니다.
@@ -23,7 +24,7 @@ public class MemberService {
     public void saveDeviceToken(Long memberId, String deviceToken) {
 
         Member member = memberRepository.findById(memberId).
-                orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER_ID));
+                orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER_ID));
 
         member.saveDeviceToken(deviceToken);
     }
@@ -31,8 +32,19 @@ public class MemberService {
     @Transactional
     public void updateMember(Long memberId, MemberUpdateRequest memberUpdateRequest) {
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER_ID));
+                .orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER_ID));
 
         member.updateMember(memberUpdateRequest);
+    }
+
+    @Transactional(readOnly = true)
+    public ProfileReadResponseDto readMemberProfile(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER_ID));
+
+        return ProfileReadResponseDto.builder()
+                .id(member.getId())
+                .name(member.getName())
+                .provider(member.getProvider().name())
+                .build();
     }
 }
