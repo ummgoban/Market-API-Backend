b3d739d4998778c259f1512ff6bf09a99c667b81
leesangmin <dkwkghkdlxld2@gmail.com>
2024-12-01 18:58:11 +0900

    refactor: 토스 결제, 요청 서버 로그 추가 #45

--- src/main/java/com/market/orders/service/PaymentService.java
+++ src/main/java/com/market/orders/service/PaymentService.java
@@ -14,6 +14,7 @@ import com.market.orders.entity.PaymentMethod;
 import com.market.orders.repository.OrdersRepository;
 import com.market.orders.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +24,7 @@ import static com.market.core.code.error.MemberErrorCode.NOT_FOUND_MEMBER_ID;
 import static com.market.core.code.error.OrdersErrorCode.NOT_FOUND_ORDERS_ID;
 import static com.market.core.code.error.PaymentErrorCode.INVALID_ORDERS_ID_AND_AMOUNT;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
@@ -38,6 +40,12 @@ public class PaymentService {
 
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER_ID));
         Orders orders = ordersRepository.findById(ordersId).orElseThrow(() -> new OrdersException(NOT_FOUND_ORDERS_ID));
+
+        log.info("수신한 ordersId = " + ordersId);
+        log.info("기존 저장된 ordersId = " + orders.getId());
+
+        log.info("수신한 금액 = " + amount);
+        log.info("기존 저장된 주문 금액 = " + orders.getOrdersPrice());
 
         if (orders.getOrdersPrice() != amount) {
             throw new PaymentException(INVALID_ORDERS_ID_AND_AMOUNT);
